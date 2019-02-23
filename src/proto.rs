e6de4e4a592e5f40ef4c8b264073dd5d1b15cc93
matthiasseitz <matthias.seitz@tum.de>
2019-02-23 23:39:50 +0100

    add response type

---
+++ src/proto.rs
@@ -0,0 +1,56 @@
+use crate::model::Work;
+
+/// Represents the crossref response for a `work` request.
+/// requesting an url: `https://api.crossref.org/works/10.1037/0003-066X.59.1.29/agency`
+/// will return following result:
+/// rust-norun```{
+///  status: "ok",
+///  message-type: "work-agency",
+///  message-version: "1.0.0",
+///  message: {
+///    DOI: "10.1037/0003-066x.59.1.29",
+///    agency: {
+///      id: "crossref",
+///      label: "Crossref"
+///    }
+///  }```
+///
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct CrossrefResponse {
+    /// the status of the request
+    pub status: String,
+    /// the type of the response message holds
+    pub message_type: MessageType,
+    /// the version of the service created this message
+    pub message_version: String,
+    // the actual message of the response
+    //    pub message: Option<Work>,
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum MessageType {
+    Single(String),
+    List(Vec<String>),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::*;
+    #[test]
+    fn serde_response() {
+        let section = r#"{
+  "status": "ok",
+  "message-type": "work-agency",
+  "message-version": "1.0.0"
+}"#;
+        let ref_type: CrossrefResponse = serde_json::from_str(section).unwrap();
+
+        assert_eq!(
+            MessageType::Single("work-agency".to_string()),
+            ref_type.message_type
+        )
+    }
+}
